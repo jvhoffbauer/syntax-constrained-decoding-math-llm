3afd6ce12f216fbb730fb19b2d4aeb1ba4e33b68
JVH <jv@lewe-hoffbauer.de>
2023-12-07 17:49:49 +0100

    Build interleaving multihop generation (wip)

--- toolmode/cfg_decoding/__init__.py
+++ toolmode/cfg_decoding/__init__.py
@@ -48,7 +48,11 @@ class CfgDecoder:
         output_text = output_text[0]
         return output_text
 
-    def generate(self, prompt: str, max_new_tokens: int):
+    def generate_free(self, prompt: str, max_new_tokens: int, target_sequence: str = "<T>"):
+        """
+        Generate free text until the target sequence was generated or max_new_tokens is reached
+        Return (only) the generated text including the target sequence
+        """
         input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
         bos_ids = torch.ones((1, 1), dtype=torch.long) * self.model.config.bos_token_id
         input_ids = torch.cat([bos_ids, input_ids], dim=-1)
@@ -66,13 +70,56 @@ class CfgDecoder:
             top_p=0,
             # Stop on first <T>
             stopping_criteria=stopping_criteria.TargetSequencetoppingCriteria(
-                target_sequence="<T>", prompt=prompt, tokenizer=self.tokenizer
+                target_sequence=target_sequence, prompt=prompt, tokenizer=self.tokenizer
             ),
         )
 
         output_text = self.tokenizer.batch_decode(output_ids[:, prompt_end_index:], skip_special_tokens=True)
         output_text = output_text[0]
 
-        output_text = output_text.replace("<T>", "")
-
         return output_text
+
+    def generate_interleaving(self, prompt: str, max_new_tokens: int, max_operations: int, use_constrained: bool): 
+        
+        answer_parts = []
+        current_prompt = prompt
+        last_result = None
+
+        num_operations = 0
+        while num_operations < max_operations:
+
+            # First generate free text. This will stop on the first <T> or when max_new_tokens is reached
+            # The answer is the free text up to the first <T>, including the <T>
+            free_text = self.generate_free(current_prompt, max_new_tokens)
+
+            # Update the prompt
+            current_prompt += free_text
+
+            # Ggenerate constrained text until max_operations is reached or the model stops generating
+            constrained_text = self.generate_constrained(current_prompt, max_new_tokens)
+
+            # Extract the operation: After the last <T>, excluding the (last) "="
+            operation = result.split("<T>")[-1].replace("=", "")
+
+            # Evaluate the constrained text (as it is a formula)
+            result = funcqa_runner.evaluate_toolcall(operation)
+
+            # Add the constrained text to the prompt
+            current_prompt += constrained_text + "=" + result
+
+            answer_parts += [
+                ("free", free_text),
+                ("constrained", constrained_text),
+                ("result", result),
+            ]
+
+            num_operations += 1
+            last_result = result
+
+        
+
+            
+
+
+            
+
