afe95f719e6a81c9c02b1310cb445af99a91c7af
JVH <jv@lewe-hoffbauer.de>
2023-12-07 15:25:48 +0100

    Change dataset code to load multihop dataset

--- toolmode/data/funcqa/__init__.py
+++ toolmode/data/funcqa/__init__.py
@@ -3,50 +3,33 @@ import os
 import datasets as ds
 import pandas as pd
 
-FUNCQA_DATASET_FILE_PATH = "data/funcqa/funcqa_oh.json"
-FUNCQA_PROMPT_TEMPLATE_FILE_FMT = "data/funcqa/template_oh/llama_<{operation}>.txt"
+FUNCQA_MULTI_HOP_DATASET_FILE_PATH = "data/funcqa/funcqa_mh.json"
+FUNCQA_PROMPT_MULTI_HOP = "data/funcqa/template_mh/llama_general_tooldec.txt"
 DATASETS_CACHE_DIR_PATH = "/scratch1/redditqa/cached_datasets"
 
 
-def get_funcqa_dataset():
-    # Load the dataset
-    dataset = ds.Dataset.from_pandas(pd.read_json(FUNCQA_DATASET_FILE_PATH, orient="records"))
+def get_funcqa_multi_hop_dataset():
+    # Load the dataset 
+    dataset = ds.Dataset.from_pandas(pd.read_json(FUNCQA_MULTI_HOP_DATASET_FILE_PATH, orient="records"))
 
-    # Save and load the dataset to enable caching
-    cache_dir = os.path.join(DATASETS_CACHE_DIR_PATH, os.path.basename(FUNCQA_DATASET_FILE_PATH))
-    dataset.save_to_disk(cache_dir)
-    dataset = ds.load_from_disk(cache_dir)
+    # # Save and load the dataset to enable caching
+    # cache_dir = os.path.join(DATASETS_CACHE_DIR_PATH, os.path.basename(FUNCQA_MULTI_HOP_DATASET_FILE_PATH))
+    # dataset.save_to_disk(cache_dir)
+    # dataset = ds.load_from_disk(cache_dir)
 
     return dataset
 
 
-def create_prompt(row):
-    # Get first text in angle brackets, i.e. <TEXT>
-    operation = row["func"].split("<")[1].split(">")[0]
-    row["operation"] = operation
-
-    # Get the prompt
-    prompt_template_file_path = FUNCQA_PROMPT_TEMPLATE_FILE_FMT.format(operation=operation)
-    with open(prompt_template_file_path, "r") as f:
-        prompt_template = f.read()
-    prompt = prompt_template.replace("[QUESTION]", row["question"])
-    row["prompt"] = prompt
-
-    return row
-
-
 def change_operation_format(row):
-    cols_to_change = ["func", "prompt"]
-    for col in cols_to_change:
-        row[col] = row[col].replace("<", "[T]").replace(">", "").replace("[T]", "<T>")
-        # Avoid replacing instructions
-        row[col] = row[col].replace(f"the operator <T>{row['operation']}", f"the operator {row['operation']}")
+    calculations = []
+    for calculation in row["calculation"]: 
+        # Replace < with [T] to avoid <T> being interpreted as a tag
+        calculation = calculation.replace("<", "[T]").replace(">", "").replace("[T]", "<T>")
+        calculations.append(calculation)
     return row
 
 
 def load():
-    dataset = get_funcqa_dataset()
-    dataset = dataset.map(create_prompt, batched=False)
-    # Disable caching for this step
+    dataset = get_funcqa_multi_hop_dataset()
     dataset = dataset.map(change_operation_format, batched=False)
     return dataset
