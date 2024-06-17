9dddfc2e3e0ee716759c649aaced7eccae5c479a
JVH <jv@lewe-hoffbauer.de>
2024-06-17 21:42:14 +0200

    Add documentaiton and cleanup

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# MIDL WS 2023: Syntax error-free decoding for Agents
+# FuncQA Experiments: Syntax error-free decoding for Math Equations
 
-Next steps the ToolDec part of the following paper (without fine-tuning of ToolDec approach yet)
-* Reproduce ![table](./table%20to%20reproduce.png)
+This repo shows two methods for syntax error-free decoding 
+
+ - using a context-free grammar (defined with lark)
+ - using a finite-state automtata that is evaluated directly on the GPU
+
+It uses these approaches to improve the performance of the Zephyr 7B LLM on FuncQA, a math equation benchmark. This approach outperforms current state-of-the-art. 
+
+It was developed as part of a seminar at [HPI](hpi.de). Here are additional resources 
+
+ - [**Slides**](./docs/slides.pdf) outlining the basic idea
+  - [**Report**](./docs/report.pdf) outlining the approach in-depth
+
+
+ ## Literature
+
+The methods implemented here are inspiered by the following two papers 
+
+1. ToolDec: Syntax Error-Free and Generalizable Tool Use for LLMs via Finite-State Decoding [[arxiv]](https://arxiv.org/pdf/2310.07075.pdf)
+2. ToolkenGPT: Augmenting Frozen Language Models with Massive Tools via Tool Embeddings [[arxiv]](https://arxiv.org/abs/2305.11554)
+
+
---
+++ scripts/eval_commands.sh
@@ -0,0 +1,26 @@
+# Stop on error
+set -e
+
+export CUDA_VISIBLE_DEVICES=0
+
+# # Untrained model, different configs
+# python3 -m toolmode.run --output_dir=eval_reports/zephyr_untrained_16bit --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="16bit" 
+# python3 -m toolmode.run --output_dir=eval_reports/zephyr_untrained_8bit --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="8bit" 
+# python3 -m toolmode.run --output_dir=eval_reports/zephyr_untrained_4bit --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="4bit" 
+
+# # Untrained model without constrains
+# python3 -m toolmode.run --output_dir=eval_reports/zephyr_untrained_16bit_unconstrained --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="16bit"  --do_not_use_constained
+
+# # Different trained models
+# python3 -m toolmode.run --output_dir=eval_reports/zephyr_trained_4bit  --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="4bit" --adapter_name=models/zephyr_funcqa_4bit/final_checkpoint
+# python3 -m toolmode.run --output_dir=eval_reports/zephyr_trained_16bit  --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="16bit" --adapter_name=models/zephyr_funcqa_16bit/final_checkpoint
+
+# # Model with GSM8k
+# python3 -m toolmode.run --output_dir=eval_reports/zephyr_trained_16bit_gsm8k  --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="16bit" --adapter_name=models/zephyr_funcqa_16bit_gsm8k/output/checkpoint-470
+
+# python3 -m toolmode.run --output_dir=eval_reports/zephyr_funcqa_16bit_endtoken  --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="16bit" --adapter_name=models/zephyr_funcqa_16bit_endtoken/output/checkpoint-80
+# python3 -m toolmode.run --output_dir=eval_reports/zephyr_funcqa_16bit_endtoken_gsm8k  --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="16bit" --adapter_name=models/zephyr_funcqa_16bit_endtoken_gsm8k/output/checkpoint-430
+
+# Eval models on GSM8k
+python3 -m toolmode.run --output_dir=eval_reports/gsm8k_test/zephyr_trained_16bit_gsm8k --dataset=gsm8k --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="16bit" --adapter_name=models/zephyr_funcqa_16bit_gsm8k/output/checkpoint-470
+python3 -m toolmode.run --output_dir=eval_reports/gsm8k_test/zephyr_trained_16bit --dataset=gsm8k --model_name="HuggingFaceH4/zephyr-7b-beta" --quantization="16bit" --adapter_name=models/zephyr_funcqa_16bit/final_checkpoint
